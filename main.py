18585f35622552485b913537d7a3b5aee5e6957c
p4trickpe <120168763+p4trickpe@users.noreply.github.com>
2022-12-12 12:07:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import sys
 
-def func(value):
+def is_even(value):
   if value % 2 == 0:
     print(f"Value {value} is even")
+    return True
   else:
     print(f"Value {value} is odd")
+    return False
 
 
 user_input = int(sys.argv[1])
-print("Hello World!")
-print(str(user_input))
-for i in range(user_input):
-  func(i)
+result = is_even(user_input)
+return result
+# print("Hello World!")
+
+# print(str(user_input))
+# for i in range(user_input):
+#   func(i)
