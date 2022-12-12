8d49d73375762b3ada7fe80f95934a47f257e9a3
p4trickpe <120168763+p4trickpe@users.noreply.github.com>
2022-12-12 11:57:49 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+import sys
+
 def func(value):
   if value % 2 == 0:
     print(f"Value {value} is even")
   else:
     print(f"Value {value} is odd")
 
-#user_input = input()
+
+user_input = sys.argv
 print("Hello World!")
+print(str(user_input))
